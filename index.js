467cbc8c574f85cc9a0e9c565ba0648c5254bae3
tahseen447 Siddiqui <tahseenfatima1986@gmail.com>
2018-11-14 20:10:58 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 //Your code here
+function justInvoke(fn) {
+  return fn();
+}
+
+function setThisWithCall(fn, name, age){
+  return fn.call(name, age);
+}
+
+function setThisWithApply(fn, thisValue, args){
+  return fn.apply(thisValue, args);
+}
+
+function returnNewFunctionOf(fn, thisValue){
+  let newFunc = fn.bind(thisValue);
+  return newFunc;
+}
